c3ba1f389494cc3c769585f5f07d616714ab6a3c
GitHudsonLima <hudsonoliveiral@outlook.com>
2024-02-26 10:48:54 -0500

    main pag layout

--- src/components/Tweet.tsx
+++ src/components/Tweet.tsx
@@ -1,15 +1,33 @@
-interface TweetProps {
-  user: string;
-  children: string;
-  likes: number;
-}
+import { ArrowsClockwise, ChatCircle, Heart } from "@phosphor-icons/react";
+import "./Tweet.css";
 
-export function Tweet(props: TweetProps) {
+export function Tweet() {
   return (
-    <div className="tweet">
-      <strong>{props.user}</strong>
-      <p>{props.children}</p>
-      <button>Like {props.likes ?? 0}</button>
-    </div>
+    <a href="#" className="tweet">
+      <img src="https://github.com/GitHudsonLima.png" alt="Hudson Lima" />
+
+      <div className="tweet-content">
+        <div className="tweet-content-header">
+          <strong>Hudson Lima</strong>
+          <span>@limahudson</span>
+        </div>
+
+        <p>I am so happy with code create-react-app for Vite and its results</p>
+        <div className="tweet-content-footer">
+          <button type="button">
+            <ChatCircle />
+            20
+          </button>
+          <button type="button">
+            <ArrowsClockwise />
+            20
+          </button>
+          <button type="button">
+            <Heart />
+            20
+          </button>
+        </div>
+      </div>
+    </a>
   );
 }
